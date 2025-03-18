f8a7bf77f9330a8f918041424d42fa398fb39109
Rekvizit <73797503@mail.ru>
2025-03-18 09:16:52 +0300

    add license

--- include/Config.hpp
+++ include/Config.hpp
@@ -1,3 +1,19 @@
+/*
+Copyright 2025 RKviZitNT
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 #pragma once
 
 #include <iostream>
@@ -7,8 +23,8 @@ namespace glbl {
     constexpr float rad = pi / 2 - 0.00001f;
 
     namespace window {
-        constexpr int width = 1920;
-        constexpr int height = 1080;
+        constexpr int width = 1200;
+        constexpr int height = 800;
 
         constexpr int frameRate = 10000;
     }
